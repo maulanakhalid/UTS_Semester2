8f8bb3f8ce0573b0cff2e90505c023428a90f1f6
maulanakhalid <117795013+maulanakhalid@users.noreply.github.com>
2023-05-11 18:42:53 +0700

    Add files via upload

---
+++ header1.php
@@ -0,0 +1,17 @@
+<meta chaiset="utf-8">
+    <meta http-equiv="X-UA-Compatible" conten="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo APPNAME;?></title>
+    
+    <!--Bootsrap-->
+
+    <link rel="stylesheet" href="./bootstrap/css/bootsrap.css">
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./sweetalert/css/sweetalert.css">
+
+    <!--JQuery and Bootstrap Scripts Loading-->
+    
+    <script src="./jQuery/jquery-3.5.1.js"></script>
+    <script src="./bootstrap/js/bootstrap.js"></script>
+    <script src="./bootstrap/js/bootstrap.min.js"></script>
+    <script src="./sweetalert/sweetalert.min.js"></script>
